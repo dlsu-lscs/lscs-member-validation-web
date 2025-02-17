65b3cd2008d10a5c6c21ef6e837dc28a3a65ac23
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2025-02-17 10:50:56 +0800

    added user details on navbar

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -4,12 +4,19 @@ const NavBar = () => {
   return (
     <>
       <div className="flex justify-center p-8 bg-black text-white">
-        <div className="flex flex-col items-center">
+        <div className="flex flex-col items-center space-y-1.5">
           <img src={lscsLogo} alt="lscsLogo" className="w-36" />
           <h1 className="text-3xl font-bold">LSCS Member Validation</h1>
-          <p className="opacity-75 py-1">
-            Verify Student LSCS Membership Status
-          </p>
+          <p className="opacity-50">Verify Student LSCS Membership Status</p>
+          {/* <div className="py-3">
+            <p>
+              <span className="font-bold">User:</span> Max Benedict Chavez
+            </p>
+            <p>
+              <span className="font-bold">Position:</span> Associate Vice
+              President
+            </p>
+          </div> */}
         </div>
       </div>
     </>
